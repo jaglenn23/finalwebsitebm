832c3c5009576c3e7e1da7b31701d40c5b5f751a
jaglenn23 <jasmineglenn2023@gmail.com>
2025-04-28 03:40:59 -0400

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,16 @@
+var x = document.getElementById("img");
+
+// if (x) {
+  x.addEventListener("mouseover", func, false);
+  x.addEventListener("mouseout", func1, false);
+// }
+
+// function func()
+// {
+//    document.getElementById("toto").setAttribute("style", "display:block;")
+// }
+
+// function func1()
+// {
+//     document.getElementById("toto").setAttribute("style", "display:none;")
+// }
